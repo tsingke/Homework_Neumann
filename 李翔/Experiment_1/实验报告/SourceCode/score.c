12c5089fa435ad9ae5bf549b2e1651b8cd2168b6
LeeFlash <592690115@qq.com>
2018-09-23 14:16:36 +0800

    revise again

--- 李翔/Experiment_1/实验报告/SourceCode/score.c
+++ 李翔/Experiment_1/实验报告/SourceCode/score.c
@@ -138,8 +138,80 @@ void printOut(SS stu[], int N)
 	for (int i = 0; i < N; i++)
 	{
 
-		printf("第%d名信息 学号：%s	姓名:%s		总成绩:%4.2f分\n", i + 1, &(stu[i].number[0]), &(stu[i].name[0]), stu[i].generalScore);
+		cout << "学号;" << stu[i].number << " " << "姓名:" << stu[i].name << " " << "平时成绩：" << stu[i].dailyScore << " " << "实验成绩:" << stu[i].text << " " << "期末成绩:" << stu[i].finalScore << " " << "总成绩:" << stu[i].generalScore << " " << "排名" << stu[i].rank << endl;
 	}
+
+}
+
+//写入Excel文档
+void putintoExcel(SS stu[],int N)
+{
+	cout << endl << "-------第四步：将学生完整信息写入项目目录下的Ecxel文档------" << endl;
+	ofstream fout;
+	fout.open("..\\score.xls");
+	fout << "学号\t姓名\t平时分数\t实验分数\t期末分数\t总分\t名次\t\n";
+	for (int i = 0; i < N; i++)
+	{
+		fout<<stu[i].number<<"\t";
+		fout << stu[i].name << "\t";
+		fout << stu[i].dailyScore << "\t";
+		fout << stu[i].finalScore << "\t";
+		fout << stu[i].text<<"\t";
+		fout << stu[i].generalScore << "\t";
+		fout << stu[i].rank<<"\t" ;
+		fout << "\n";
+	}
+	fout.close();
+
+}
+
+//输入学号即可查询功能
+void inSpect(SS stu[],int N)
+{
+	long a;
+	int b=0;
+	char f=0;
+	cout << endl << "————————————第五步：查询功能——————————" << endl;
+	cout << "请输入要查询学号：" << endl;
+
+	
+		cin >> a;
+	
+	for (int i = 0; i <N; i++)
+	{
+		if (a==stu[i].number)
+		{
+			cout << "符合学号;" << stu[i].number << "的是 " << "姓名:" << stu[i].name << " " << "平时成绩：" << stu[i].dailyScore << " " << "实验成绩:" << stu[i].text << " " << "期末成绩:" << stu[i].finalScore << " " << "总成绩:" << stu[i].generalScore << " " << "排名" << stu[i].rank << endl;
+			b = 1;
+		
+		}
+			
+	}
+
+	if (b == 0)cout << "查询失败" << endl;
+}
+//求方差及平均值
+void calculate(SS stu[],int N)
+{
+	int i = 0;
+	cout << endl << "————————————第六步：计算均值——————————" << endl;
+	float SUM=0;
+	for (; i < N; i++)
+	{
+		SUM = SUM+stu[i].generalScore;
+	}
+	SUM = (float)(SUM / i);
+	cout << "平均总成绩为：";
+	cout<<SUM << endl;
+	cout << endl << "————————————第七步：计算方差——————————" << endl;
+	float b=0, c;
+	i = 0;
+	for (; i < N; i++)
+	{
+		b = (float)(b + (stu[i].generalScore - SUM )*(stu[i].generalScore - SUM ));
+	}
+	c = (float)(b / i);
+	cout << "方差为：" << c << endl;
 
 
 }
