72b1a443045bef14cb8eca9f97a3031a6e0981c9
merry <201711010415@stu.stdu.edu.cn>
2018-09-28 18:36:12 +0800

    motify my homework

---
+++ 王金雨/Experiment_2/answear 7.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int add(int a,int b);
+int main()
+{
+   int x,y,sum;
+   cout<<"Please input x and y:\n"<<endl;
+   cin>>x>>y;
+   sum=add(x,y);
+   cout<<x<<"+"<<y<<"="<<sum;
+   return 0;
+}
+ int add(int a,int b)
+{
+    return a+b;
+}
