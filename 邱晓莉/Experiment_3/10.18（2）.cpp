73625b043aff23c46ad7b916d54e7e8c14be95a3
201711010319 <201711010319@stu.sdnu.edu.cn>
2018-10-20 14:10:36 +0800

    10-18修改

--- 邱晓莉/Experiment_3/10.18（2）.cpp
+++ 邱晓莉/Experiment_3/10.18（2）.cpp
@@ -1,40 +1,40 @@
 #include<iostream>
 using namespace std;
 
-void cheakAgeScore(int &age, float &score)
+float cheakAgeScore(int &age, float &score)
 {
 
 	if (age <= 16 || age >= 25)
 		throw age; //如果该同学年龄小于16岁或者大于25岁，则抛出异常
-	
+
 	if (score <= 0 || score >= 5)
 		throw score;//如果该同学成绩低于0或者大于5，则抛出异常
-	
-	
+	return score;
+
 }
 
 int main()
 {
-	void cheakAgeScore(int age, float score);
+	float cheakAgeScore(int age, float score);
 	char name[10];
 	int age;
 	float score;
 
 	cout << "请输入该同学的姓名、年龄、五级制C++语言考试分数" << endl;
 	cin >> name >> age >> score;
-	
-	
+
+
 	try     //检查是否出异常
 	{
-		
+
 		cout << "姓名：" << name;
-		void cheakAgeScore(int age,float score);
+		cheakAgeScore(age,score);
 		cout << "    年龄：" << age;
-		cout<< "   五级制C++语言考试分数：" << score << endl;
-		
-		
+		cout<< "   五级制C++语言考试分数：" <<cheakAgeScore(age,score) << endl;
+
+
 	}
-	
+
 	catch (int)   //捕获异常并作出处理，即输出一条提示信息
 	{
 		cout << "年龄输入不规范" << endl;
@@ -46,7 +46,7 @@ int main()
 
 
 
-	
+
 
 	system("pause");
 	return 0;
