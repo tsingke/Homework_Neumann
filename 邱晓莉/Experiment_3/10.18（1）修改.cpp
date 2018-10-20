7db15aaaac6710d52b7180bde32294d0f081bfb9
201711010319 <201711010319@stu.sdnu.edu.cn>
2018-10-20 20:34:34 +0800

    10.18修改

--- 邱晓莉/Experiment_3/10.18（1）修改.cpp
+++ 邱晓莉/Experiment_3/10.18（1）修改.cpp
@@ -1,4 +1,4 @@
-#include<iostream>
+	#include<iostream>
 using namespace std;
 
 int main()
@@ -10,29 +10,48 @@ int main()
 	for (i = 0; i < 20; i++)
 	{
 		cin >> ptr[i];
-		
+
 		if (ptr[i] < 0)//如果输入的数小于0，a+1
 			a++;
 		if (ptr[i] > 0)
 			b++;//如果输入的数大于0，b+1
      }
-	cout << "正数个数为：" << a << endl;
-	cout << "负数个数为：" << b << endl;
-	int m, s, sum = 0,sum2=0;//定义平均数为m,方差为s，sum为这20个数的和
+	cout << "负数个数为：" << a << endl;
+	cout << "正数个数为：" << b << endl;
+	int m, s, sum = 0,sum2=0;//定义平均数为m,方差为s，sum为这20个数的和，sum2为20个数与平均值差的平方的总和
 	for (i = 0; i <20; i++)
 		sum = sum + ptr[i];
 
-		m = sum / 20;
+		m = sum /20;
 		cout << "这20个数的平均值为：" << m << endl;
 	for (i = 0; i <20; i++)
 		sum2 =sum2+ (ptr[i] - m)*(ptr[i] - m);
 	s = sum2 / 20;
 	cout << "这20个数的方差为：" << s << endl;
+int j,t;
+	for (i = 0; i<20; i++)
+	{
+	    for(j=i+1;j<20;j++)
+		{
+		    if (ptr[i]>ptr[j])
+            {
+                t = ptr[j];
+                ptr[j] = ptr[i];
+                ptr[i] = t;
+
+            }
+		}
+	}
+	cout << "这20个数从小到大的排序为：" << endl;
+	for (i = 0; i < 20; i++)
+		cout << ptr[i] << endl;
 
 
 
 
 		delete[]ptr;
-	system("pause");
+
 	return 0;
 }
+
+
