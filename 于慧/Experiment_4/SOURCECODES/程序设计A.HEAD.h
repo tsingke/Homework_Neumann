9a2b4d5473d05386897516d47b03300c89b260e0
yuhuii <201711010406@stu.sdnu.edu.cn>
2018-10-21 16:21:36 +0800

    第四次实验作业

---
+++ 于慧/Experiment_4/SOURCECODES/程序设计A.HEAD.h
@@ -0,0 +1,13 @@
+#ifndef HEAD_H_INCLUDED
+#define HEAD_H_INCLUDED
+
+#include<iostream>
+using namespace std;
+
+const int n = 20;
+
+void PandDnumber(int *p);//计算正负值个数
+void calculate(int *p);//求方差及平均值
+void sortScore(int *p);//排名
+
+#endif // HEAD_H_INCLUDED
